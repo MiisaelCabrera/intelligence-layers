2ffd169fc8add8a7a20aadaec9775d639ebf30da
Misael Cabrera <misael.cabrera.es@gmail.com>
2025-11-22 18:44:24 -0600

    Connection test

--- api/src/index.ts
+++ api/src/index.ts
@@ -9,6 +9,22 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 const FRONTEND_URL = process.env.FRONTEND_URL || "*";
 
+const allowedOrigins = [FRONTEND_URL, "localhost:3000"];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        return callback(new Error("CORS not allowed for this origin"));
+      }
+    },
+  })
+);
+
 // CORS
 app.use(
   cors({
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,6 +1,24 @@
+"use client";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const API_URL = process.env.NEXT_PUBLIC_API_URL;
+  const [message, setMessage] = useState("");
+
+  const fetchTestMessage = async () => {
+    const res = await fetch(`${API_URL}/api/hello`, {
+      cache: "no-store",
+    });
+    const data = await res.json();
+    const message = data.message;
+    setMessage(message);
+  };
+
+  useEffect(() => {
+    fetchTestMessage();
+  }, []);
+
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
@@ -12,6 +30,7 @@ export default function Home() {
           height={20}
           priority
         />
+        <p>{message}</p>
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
             To get started, edit the page.tsx file.
